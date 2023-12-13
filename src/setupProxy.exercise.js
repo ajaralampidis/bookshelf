c4a3d3e10041b38894f65baf7094d33c648a64ca
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-13 20:34:52 -0300

    routing extra credit 02

--- src/setupProxy.exercise.js
+++ src/setupProxy.exercise.js
@@ -1,19 +1,5 @@
-// 🐨 you don't need to do anything in this file for the exercise. This is
-// just here for the extra credit. See the instructions for more info.
-
-
-// `get` to handle requests
-// sent to a URL matching this regex: `/\/$/` and redirects to `/discover`.
-
 function proxy(app) {
-    // add the redirect handler here
-    app.get(/\/$/, function(req, res) {
-        res.redirect("/discover")
-    });
-  }
-  
+  app.get(/^\/$/, (req, res) => res.redirect('/discover'))
+}
+
 module.exports = proxy
-
-
-
-// module.exports = () => {}
