d7d95209d62926f89fd9a18e301b324f9dccc0ec
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-13 14:24:05 -0300

    data fetching extra credit 1

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,60 +1,47 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
+import * as colors from "styles/colors"
 
-import './bootstrap'
+import * as React from 'react'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch, FaTimes} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
 import {client} from './utils/api-client'
-import {useEffect, useState} from 'react'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const [status, setStatus] = useState('idle')
-  const [data, setData] = useState({})
-  const [query, setQuery] = useState('')
+  const [status, setStatus] = React.useState('idle')
+  const [data, setData] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [query, setQuery] = React.useState('')
+  const [queried, setQueried] = React.useState(false)
 
-  // const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
-  const [queried, setQueried] = useState(false)
-
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
-  useEffect(() => {
-    if (!queried) return
-    setStatus('loading')
-    client(`books?query=${encodeURIComponent(query)}`).then(data => {
-      // if (!data.ok) {
-      //   setStatus('error')
-      //   return Promise.reject(data)
-      // }
-      // console.log("data",data)
-      setData(data)
-      setStatus('success')
-    })
-  }, [queried, query])
-
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = status === 'lodaing'
+  const isIdle = status === 'idle'
+  const isLoading = status === 'loading'
   const isSuccess = status === 'success'
+  const isError = status === 'error'
+
+  React.useEffect(() => {
+    if (!queried) {
+      return
+    }
+    setStatus('loading')
+    client(`books?query=${encodeURIComponent(query)}`).then(
+      responseData => {
+        setData(responseData)
+        setStatus('success')
+      },
+      error => {
+        setError(error)
+        setStatus('error')
+      },
+    )
+  }, [query, queried])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
     event.preventDefault()
-    // 🐨 set the queried state to true
     setQueried(true)
-    // 🐨 set the query value which you can get from event.target.elements
-    // console.log("event.target.elements", event.target.elements["search"].value)
-    setQuery(event.target.elements['search'].value)
-    // 💰 console.log(event.target.elements) if you're not sure.
+    setQuery(event.target.elements.search.value)
   }
 
   return (
@@ -78,11 +65,20 @@ function DiscoverBooksScreen() {
                 background: 'transparent',
               }}
             >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
+              {isLoading && <Spinner />}
+              {isError && <FaTimes aria-label="error" css={{color: colors.danger}} />}
+              {(isSuccess || isIdle) && <FaSearch aria-label="search" />}
             </button>
           </label>
         </Tooltip>
       </form>
+
+      {isError ? (
+        <div css={{color: colors.danger}}>
+          <p>There was an error:</p>
+          <pre>{error.message}</pre>
+        </div>
+      ) : null}
 
       {isSuccess ? (
         data?.books?.length ? (
