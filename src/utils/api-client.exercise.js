e971e888fae0279557aa02c8a758f11a47d02c56
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-13 18:02:27 -0300

    authentication extra credit 03

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,19 +1,26 @@
+import * as auth from 'auth-provider'
+
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
   {token, headers: customHeaders, ...customConfig} = {},
 ) {
-    const config = {
-      headers: {
-        Authorization: token ? `Bearer ${token}` : undefined,
-        ...customHeaders,
-      },
-      ...customConfig,
-    }
+  const config = {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      ...customHeaders,
+    },
+    ...customConfig,
+  }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+    if(data.status === 401) {
+      auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
     if (response.ok) {
       return data
     } else {
