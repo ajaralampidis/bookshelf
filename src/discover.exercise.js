b6ef03fc01a9720302358b35ff92061d4395654b
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-13 14:02:10 -0300

    data fetching exercise

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -7,13 +7,20 @@ import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
 // 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+import {useEffect, useState} from 'react'
 
 function DiscoverBooksScreen() {
   // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
+  const [status, setStatus] = useState('idle')
+  const [data, setData] = useState({})
+  const [query, setQuery] = useState('')
+
+  // const data = null // 💣 remove this, it's just here so the example doesn't explode
   // 🐨 you'll also notice that we don't want to run the search until the
   // user has submitted the form, so you'll need a boolean for that as well
   // 💰 I called it "queried"
+  const [queried, setQueried] = useState(false)
 
   // 🐨 Add a useEffect callback here for making the request with the
   // client and updating the status and data.
@@ -21,15 +28,32 @@ function DiscoverBooksScreen() {
   // 🐨 remember, effect callbacks are called on the initial render too
   // so you'll want to check if the user has submitted the form yet and if
   // they haven't then return early (💰 this is what the queried state is for).
+  useEffect(() => {
+    if (!queried) return
+    setStatus('loading')
+    client(`books?query=${encodeURIComponent(query)}`).then(data => {
+      // if (!data.ok) {
+      //   setStatus('error')
+      //   return Promise.reject(data)
+      // }
+      // console.log("data",data)
+      setData(data)
+      setStatus('success')
+    })
+  }, [queried, query])
 
   // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  const isLoading = status === 'lodaing'
+  const isSuccess = status === 'success'
 
   function handleSearchSubmit(event) {
     // 🐨 call preventDefault on the event so you don't get a full page reload
+    event.preventDefault()
     // 🐨 set the queried state to true
+    setQueried(true)
     // 🐨 set the query value which you can get from event.target.elements
+    // console.log("event.target.elements", event.target.elements["search"].value)
+    setQuery(event.target.elements['search'].value)
     // 💰 console.log(event.target.elements) if you're not sure.
   }
 
