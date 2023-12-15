b63bbac18f9918ffa6dfaaba39b27df879b077ae
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-15 19:35:57 -0300

    context exercise

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,16 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext()
+
+
+// const AuthContextProvider = ({children, value}) => {
+//     return (
+//         <AuthContext.Provider value={value} >
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// }
+
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
