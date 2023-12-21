32c67295c71ddba8ccab9708aa7bb913ac268625
Agustin Jaralampidis <ajaralampidis@gmail.com>
2023-12-21 15:06:39 -0300

    integration testing extra exercise 3

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,14 +1,43 @@
 import * as React from 'react'
-import {render, screen, waitForElementToBeRemoved, within} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {queryCache} from 'react-query'
 import * as auth from 'auth-provider'
 import {buildUser, buildBook} from 'test/generate'
 import * as usersDB from 'test/data/users'
 import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
+import {formatDate} from 'utils/misc'
 import {AppProviders} from 'context'
 import {App} from 'app'
-import userEvent from '@testing-library/user-event'
+
+// > 💰 In my solution I have a custom `render`, a `loginAsUser`, and a
+// > `waitForLoadingToFinish`
+
+const _render = async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'Test page', route)
+
+  const r = render(<App />, {wrapper: AppProviders})
+
+  return {book, route, r}
+}
+
+const loginAsUser = async () => {
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+  return {user, authUser}
+}
+
+const waitForLoadingToFinish = async () => {
+  await waitForElementToBeRemoved(async () => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+}
 
 // general cleanup
 afterEach(async () => {
@@ -22,21 +51,27 @@ afterEach(async () => {
 })
 
 test('renders all the book information', async () => {
-  const user = buildUser()
-  await usersDB.create(user)
-  const authUser = await usersDB.authenticate(user)
-  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+  // const user = buildUser()
+  // await usersDB.create(user)
+  // const authUser = await usersDB.authenticate(user)
+  // window.localStorage.setItem(auth.localStorageKey, authUser.token)
 
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  window.history.pushState({}, 'Test page', route)
+  // const book = await booksDB.create(buildBook())
+  // const route = `/book/${book.id}`
+  // window.history.pushState({}, 'Test page', route)
 
-  render(<App />, {wrapper: AppProviders})
+  // render(<App />, {wrapper: AppProviders})
+  
+  
+  // await waitForElementToBeRemoved(() => [
+  //   ...screen.queryAllByLabelText(/loading/i),
+  //   ...screen.queryAllByText(/loading/i),
+  // ])
 
-  await waitForElementToBeRemoved(() => [
-    ...screen.queryAllByLabelText(/loading/i),
-    ...screen.queryAllByText(/loading/i),
-  ])
+  await loginAsUser()
+  const {book} = await _render()
+  await waitForElementToBeRemoved()
+
 
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
   expect(screen.getByText(book.author)).toBeInTheDocument()
@@ -65,77 +100,54 @@ test('renders all the book information', async () => {
 })
 
 test('can create a list item for the book', async () => {
-  const user = buildUser()
-  await usersDB.create(user)
-  const authUser = await usersDB.authenticate(user)
-  window.localStorage.setItem(auth.localStorageKey, authUser.token)
-
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  window.history.pushState({}, 'Test page', route)
-
-  render(<App />, {wrapper: AppProviders})
-
-  await waitForElementToBeRemoved(() => [
-    ...screen.queryAllByLabelText(/loading/i),
-    ...screen.queryAllByText(/loading/i),
-  ])
-
-  const AddToListButton = screen.getByRole('button', {name: /add to list/i})
-  await userEvent.click(AddToListButton)
-  await waitForElementToBeRemoved(() => within(AddToListButton).queryByLabelText(/loading/i))
-
-  const markAsRead = screen.getByRole('button', {name: /mark as read/i})
-  const removeFromList = screen.getByRole('button', {name: /remove from list/i})
-
-
-  expect(markAsRead).toHaveAttribute('aria-label', "Mark as read")
-  expect(markAsRead).toBeInTheDocument()
-  expect(removeFromList).toHaveAttribute('aria-label', "Remove from list")
-  expect(markAsRead).toBeInTheDocument()
-
-  const ReadingListNav = screen.getByRole('link', {name: /reading list/i})
-  await userEvent.click(ReadingListNav)
-
-
-  const listItem = screen.getByRole('listitem', {name: book.title})
-  expect(listItem).toBeInTheDocument()
-})
-
-test('can create a list item for the book (this is in the /discover page)', async () => {
-  const user = buildUser()
-  await usersDB.create(user)
-  const authUser = await usersDB.authenticate(user)
-  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+  // const user = buildUser()
+  // await usersDB.create(user)
+  // const authUser = await usersDB.authenticate(user)
+  // window.localStorage.setItem(auth.localStorageKey, authUser.token)
 
   // const book = await booksDB.create(buildBook())
-  const route = `/discover`
-  window.history.pushState({}, 'Test page', route)
+  // const route = `/book/${book.id}`
+  // window.history.pushState({}, 'Test page', route)
 
-  render(<App />, {wrapper: AppProviders})
+  // render(<App />, {wrapper: AppProviders})
 
-  await waitForElementToBeRemoved(() => [
-    ...screen.queryAllByLabelText(/loading/i),
-    ...screen.queryAllByText(/loading/i),
-  ])
+  // await waitForElementToBeRemoved(() => [
+  //   ...screen.queryAllByLabelText(/loading/i),
+  //   ...screen.queryAllByText(/loading/i),
+  // ])
 
-  const book = screen.getByRole('listitem', {name: /the hobbit/i})
-  const AddToListButton = within(book).getByRole('button', {name: /add to list/i})
-  await userEvent.click(AddToListButton)
-  await waitForElementToBeRemoved(() => within(AddToListButton).queryByLabelText(/loading/i))
+  await loginAsUser()
+  await _render()
+  await waitForElementToBeRemoved()
 
-  const [markAsRead, removeFromList] = within(book).getAllByRole('button')
 
-  expect(markAsRead).toHaveAttribute('aria-label', "Mark as read")
-  expect(markAsRead).toBeInTheDocument()
-  expect(removeFromList).toHaveAttribute('aria-label', "Remove from list")
-  expect(markAsRead).toBeInTheDocument()
+  const addToListButton = screen.getByRole('button', {name: /add to list/i})
+  await userEvent.click(addToListButton)
+  expect(addToListButton).toBeDisabled()
 
-  const ReadingListNav = screen.getByRole('link', {name: /reading list/i})
-  await userEvent.click(ReadingListNav)
-  
-  const listItem = screen.getByRole('listitem', {name: /the hobbit/i})
-  expect(listItem).toBeInTheDocument()
-  expect(listItem.innerHTML).toMatch(book.innerHTML)
+  // await waitForElementToBeRemoved(() => [
+  //   ...screen.queryAllByLabelText(/loading/i),
+  //   ...screen.queryAllByText(/loading/i),
+  // ])
+  await waitForElementToBeRemoved()
 
+
+  expect(
+    screen.getByRole('button', {name: /mark as read/i}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /remove from list/i}),
+  ).toBeInTheDocument()
+  expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
+
+  const startDateNode = screen.getByLabelText(/start date/i)
+  expect(startDateNode).toHaveTextContent(formatDate(Date.now()))
+
+  expect(
+    screen.queryByRole('button', {name: /add to list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
 })
